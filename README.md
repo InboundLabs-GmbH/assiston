5ced19a9dba81a116961374629998f19f5b86bcb
Steve Frenzel <43616173+stevefrenzel@users.noreply.github.com>
2024-08-28 10:59:22 +0200

    Add components for testing Storyblok

---
+++ README.md
@@ -0,0 +1,7 @@
+# Assiston e.V.
+
+Website of Assiston e.V. using the following tech stack:
+
+- [Astro](https://astro.build/)
+- [Storyblok](https://www.storyblok.com/)
+- [Netlify](https://www.netlify.com/)
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,27 @@
 import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 export default defineConfig({
-  site: "https://astro-naut-template.vercel.app/",
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        blogPost: "storyblok/BlogPost",
+        blogPostList: "storyblok/BlogPostList",
+        heroSection: "storyblok/HeroSection",
+        page: "storyblok/Page",
+      },
+    }),
+  ],
+  site: "https://www.assiston.de/",
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
 });
