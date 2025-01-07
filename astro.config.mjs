c0e57b419130300694c660871d3091fd6afab3c1
Steve Frenzel <43616173+stevefrenzel@users.noreply.github.com>
2025-01-07 08:36:27 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,30 @@
 import { defineConfig } from "astro/config";
-import storyblok from "@storyblok/astro";
+// import storyblok from "@storyblok/astro";
 import { loadEnv } from "vite";
-import basicSsl from "@vitejs/plugin-basic-ssl";
-import netlify from "@astrojs/netlify";
-const env = loadEnv("", process.cwd(), "STORYBLOK");
+// import basicSsl from "@vitejs/plugin-basic-ssl";
+// import netlify from "@astrojs/netlify";
+// const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [storyblok({
-    accessToken: env.STORYBLOK_TOKEN,
-    components: {
-      blogPost: "storyblok/BlogPost",
-      blogPostList: "storyblok/BlogPostList",
-      heroSection: "storyblok/HeroSection",
-      page: "storyblok/Page"
-    }
-  })],
+  // integrations: [
+  //   storyblok({
+  //     accessToken: env.STORYBLOK_TOKEN,
+  //     components: {
+  //       blogPost: "storyblok/BlogPost",
+  //       blogPostList: "storyblok/BlogPostList",
+  //       heroSection: "storyblok/HeroSection",
+  //       page: "storyblok/Page",
+  //     },
+  //   }),
+  // ],
   site: "https://www.assiston.de/",
-  vite: {
-    plugins: [basicSsl()],
-    server: {
-      https: true
-    }
-  },
-  output: "server",
-  adapter: netlify()
+  // vite: {
+  //   plugins: [basicSsl()],
+  //   server: {
+  //     https: true,
+  //   },
+  // },
+  // output: "server",
+  // adapter: netlify(),
 });
