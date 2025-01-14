0d4e38e22564caec60923d2f9f90fc72b7bb534c
Steve Frenzel <43616173+stevefrenzel@users.noreply.github.com>
2025-01-14 12:42:58 +0100

    Update config and packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,39 @@
 import { defineConfig } from "astro/config";
-// import storyblok from "@storyblok/astro";
-// import { loadEnv } from "vite";
-// import basicSsl from "@vitejs/plugin-basic-ssl";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 // import netlify from "@astrojs/netlify";
-// const env = loadEnv("", process.cwd(), "STORYBLOK");
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
-  // integrations: [
-  //   storyblok({
-  //     accessToken: env.STORYBLOK_TOKEN,
-  //     components: {
-  //       blogPost: "storyblok/BlogPost",
-  //       blogPostList: "storyblok/BlogPostList",
-  //       heroSection: "storyblok/HeroSection",
-  //       page: "storyblok/Page",
-  //     },
-  //   }),
-  // ],
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        budgetListItem: "storyblok/BudgetListItem",
+        contactSection: "storyblok/ContactSection",
+        descriptionListItem: "storyblok/DescriptionListItem",
+        footer: "storyblok/Footer",
+        galleryItem: "storyblok/GalleryItem",
+        heroSection: "storyblok/HeroSection",
+        page: "storyblok/Page",
+        processContainer: "storyblok/ProcessContainer",
+        processItem: "storyblok/ProcessItem",
+        richtext: "storyblok/RichText",
+        serviceListItem: "storyblok/ServiceListItem",
+        teamListItem: "storyblok/TeamListItem",
+        textSection: "storyblok/TextSection",
+      },
+    }),
+  ],
   site: "https://assiston.netlify.app/",
-  // vite: {
-  //   plugins: [basicSsl()],
-  //   server: {
-  //     https: true,
-  //   },
-  // },
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
   // output: "server",
   // adapter: netlify(),
 });
