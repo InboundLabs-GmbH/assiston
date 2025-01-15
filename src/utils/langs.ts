a0214c657ce00b14460d2f43a9c0e96c07405f0f
Steve Frenzel <43616173+stevefrenzel@users.noreply.github.com>
2025-01-15 11:59:24 +0100

    Delete files

--- src/utils/langs.ts
+++
@@ -1,6 +0,0 @@
-//You can have the languages in an utils file so it can be reused.
-let languages = ["de", "en"];
-function getTransLink(language: string, slug: string) {
-  return language === "de" ? slug : `/${language}${slug}`;
-}
-export { getTransLink, languages };
